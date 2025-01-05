aecb9e9c745fd1794850df13b9101817a756447d
Batajoo <sanjivbatajoo@gmail.com>
2025-01-04 23:07:03 -0600

    react router learning

---
+++ 07reactRouter/src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer(){
+    return (
+        <>
+        <div class="bg-blue-400">About</div>
+        </>
+    )
+}
+
+export default Footer;
---
+++ 07reactRouter/src/components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function About(){
+    return (
+        <>
+        <div class="bg-red-400">About</div>
+        </>
+    )
+}
+
+export default About;
