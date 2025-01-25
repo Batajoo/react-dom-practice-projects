3eee2453f92ad3d2451bdbbb7fb683b347185730
Batajoo <sanjivbatajoo@gmail.com>
2025-01-24 20:33:10 -0600

    redux toolkit complete

--- 12reactReduxTodo/src/App.jsx
+++ 12reactReduxTodo/src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
+import Todos from './components/Todos'
+import AddTodos from './components/AddTodos'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      Learn redux toolkit
+      <AddTodos/>
+      <Todos/>
     </>
   )
 }
