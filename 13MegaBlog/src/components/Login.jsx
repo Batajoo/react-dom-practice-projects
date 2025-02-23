92c7936288440bbac9759383f4f28e4f6d9b7873
Sanjiv Thinkpad W11 <sanjivbatajoo@gmail.com>
2025-02-23 04:43:19 -0600

    login function implement in Login

--- 13MegaBlog/src/components/Login.jsx
+++ 13MegaBlog/src/components/Login.jsx
@@ -8,14 +8,35 @@ import { useForm } from 'react-hook-form'
 
 function Login() {
     // need to revise how to use redux toolkit and navigate as well and read the docs of useForm
-    const naviagate = useNavigate()
+    const navigate = useNavigate()
     const dispatch = useDispatch()
     const {register, handleSubmit} = useForm()
     const [error, setError] = useState("")
 
-    const login = async
+    const login = async (data) => {
+      setError("")
+      try {
+        const session = await authService.login(data);
+        if(session){
+          const userData = await authService.getCurrentUser();
+          if(userData) dispatch(authLogin(userData));
+          navigate("/")
+        }
+      }catch(error){
+        setError(error.message)
+      }
+    }
   return (
-    <div>Login</div>
+    <div
+      className='flex items-center justify-center w-full'>
+        <div className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}>
+          <div className='mb-2 flex justify-center'>
+            <span className='inline-block w-full max-w-[100px]'>
+              <Logo width='100%' />
+            </span>
+          </div>
+        </div>
+    </div>
   )
 }
 
