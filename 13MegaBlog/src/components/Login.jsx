7b46282cec9a83252fb33453652b3bc183730ccb
Sanjiv Thinkpad W11 <sanjivbatajoo@gmail.com>
2025-02-24 23:45:23 -0600

    Small changes

--- 13MegaBlog/src/components/Login.jsx
+++ 13MegaBlog/src/components/Login.jsx
@@ -13,7 +13,7 @@ function Login() {
     const {register, handleSubmit} = useForm()
     const [error, setError] = useState("")
     
-    // login
+    // login function
     const login = async (data) => {
       setError("")
       try {
