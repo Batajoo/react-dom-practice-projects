cb3424049e6c122bf26f0fd43b5c3ea112e7dfc8
Sanjiv <sanjivbatajoo@gmail.com>
2025-03-06 00:05:40 -0600

    rte update

--- 13MegaBlog/src/components/RTE.jsx
+++ 13MegaBlog/src/components/RTE.jsx
@@ -1,18 +1,10 @@
 import React from 'react'
 import {Editor} from '@tinymce/tinymce-react'
+import {Controller} from 'react-hook-form'
 
 export default function RTE() {
   // react rte 
   return (
-    <Editor
-        initialValue='default value'
-        init={{
-            branding: false,
-            height: 500,
-            menubar: true,
-            pluigns: []
-        }
-        }
-    />
+    <div>test</div>
   )
 }
