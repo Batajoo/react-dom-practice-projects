c1d5b8b27a18382af1a03231e251be53e5113208
Sanjiv <sanjivbatajoo@gmail.com>
2025-03-07 18:05:16 -0600

    post form complete

--- 13MegaBlog/src/components/RTE.jsx
+++ 13MegaBlog/src/components/RTE.jsx
@@ -2,9 +2,44 @@ import React from 'react'
 import {Editor} from '@tinymce/tinymce-react'
 import {Controller} from 'react-hook-form'
 
-export default function RTE() {
+export default function RTE({name, control, label, defaultValue = ""}) {
   // react rte here
   return (
-    <div>test</div>
+    <div className='w-full'>
+      {label && <label className='inline-block mb-1 pl-1'>{label}</label>}
+
+      <Controller
+        name={name}
+        control={control}
+        render={({field: {onChange}})=>(
+          <Editor
+            initialValue={defaultValue}
+            init={{
+              height: 500,
+              menubar: false,
+              plugins: [
+                "image",
+                "advlist",
+                "autolink",
+                "lists",
+                "link",
+                "image",
+                "charmap",
+                "preview",
+                "anchor",
+                "serachreplace",
+                "visualblocks"
+              ],
+              toolbar: 'undo redo | formatselect | bold italic | underline | \
+                        alignleft aligncenter alignright alignjustify | \
+                        bullist numlist outdent indent | removeformat | help',
+              content_style: "body {font-family: Helvetica, Arial, sans-serif; font-size: 14px }"
+            }}
+            onEditorChange={onChange}
+            
+          />
+        )}
+      />
+    </div>
   )
 }
--- 13MegaBlog/src/components/index.js
+++ 13MegaBlog/src/components/index.js
@@ -4,10 +4,14 @@ import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import Button from "./Button";
+import RTE from "./RTE";
 
 export {
     Header,
     Footer,
     Container,
-    Logo
+    Logo,
+    LogoutBtn,
+    Button,
+    RTE
 }
