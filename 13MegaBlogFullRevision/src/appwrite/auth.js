5a516c2d175485b161ff297b32e15e1bf5a9b124
Sanjiv <sanjivbatajoo@gmail.com>
2025-03-10 00:49:16 -0500

    create appwrite backend

--- 13MegaBlogFullRevision/src/appwrite/auth.js
+++ 13MegaBlogFullRevision/src/appwrite/auth.js
@@ -1,4 +1,4 @@
-import { Client, Account} from "appwrite"
+import { Client, Account, ID} from "appwrite"
 import conf from "../conf/conf"
 
 export class AuthService{
@@ -10,6 +10,46 @@ export class AuthService{
             .setProject(conf.appwriteProjectId);
         this.account = new Account(this.client);
 
+    }
+
+    async createAccount({email, password, name}){
+        try {
+            const userAccount = await this.account.create(ID.unique, email, password, name);
+            if (userAccount) {
+                // call another method 
+                this.login({email, password})
+            } else {
+                return userAccount;
+            }
+        }catch (error){
+            throw error;
+        }
+    }
+
+    async login({email, password}){
+        try {
+            const loginSession = await this.account.createEmailPasswordSession(email, password);
+            return loginSession;
+        } catch (error){
+            throw error;
+        }
+    }
+
+    async logout(){
+        try {
+            return await this.account.deleteSessions();
+        } catch (error){
+            throw error;
+        }
+    }
+
+    async getCurrentUser(){
+        try {
+            return await this.account.get();
+        } catch(error){
+            console.log("Appwrite service :: getCurrentUser :: error ", error);
+        }
+        return null;
     }
 }
 
