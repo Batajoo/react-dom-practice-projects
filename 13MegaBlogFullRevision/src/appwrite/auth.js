8f789d6f17b680a64a1149a4b2b514a4d6f7357c
Sanjiv <sanjivbatajoo@gmail.com>
2025-03-09 23:59:29 -0500

    new commit

---
+++ 13MegaBlogFullRevision/src/appwrite/auth.js
@@ -0,0 +1,18 @@
+import { Client, Account} from "appwrite"
+import conf from "../conf/conf"
+
+export class AuthService{
+    client = new Client();
+    account;
+    constructor(){
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        this.account = new Account(this.client);
+
+    }
+}
+
+const authService = new AuthService();
+
+export default authService;
