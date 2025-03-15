64c842d2f7f7ac560120fbdc3ed9467ae25f75aa
Sanjiv <sanjivbatajoo@gmail.com>
2025-03-14 23:47:35 -0500

    post form component done

--- 13MegaBlogFullRevision/src/components/post-form/PostForm.jsx
+++ 13MegaBlogFullRevision/src/components/post-form/PostForm.jsx
@@ -16,6 +16,7 @@ function PostForm({post}) {
     })
     const navigate = useNavigate()
     const userData = useSelector(state => state.user.userData)
+    
     const submit = async (data) => {
         if(post){
             const file = data.image[0] ? appwriteService.uploadFile(data.image[0]) : null;
@@ -23,8 +24,54 @@ function PostForm({post}) {
             if(file){
                 appwriteService.deleteFile(post.featuredImage);
             }
+            const dbPost = await appwriteService.updatePost(post.$id, {
+                ...data,
+                featuredImage: file ? file.$id : undefined,
+            })
+
+            if(dbPost){
+                navigate(`/post/${dbPost.$id}`)
+            }
+        } else {
+            const file = await appwriteService.uploadFile(data.image[0])
+
+            if(file){
+                const fileId = file.$id;
+                data.featuredImage = fileId;
+                const dbPost = await appwriteService.createPost({
+                    ...data,
+                    userId: userData.$id,
+
+                });
+
+                if(dbPost){
+                    navigate(`/post/${dbPost.$id}`)
+                }
+            }
         }
     }
+
+    const slugTransform = useCallback((value)=>{
+        if(value && typeof value === "string") {
+            return value
+                    .trim()
+                    .toLowerCase()
+                    .replace(/^[a-xA-Z\d\s]+/g, '-')
+                    .replace(/\s/g, "-")
+        }
+        return ''
+    },[])
+
+    React.useEffect(()=>{
+        const subscription = watch((value, {name})=>{
+            if(name === 'title'){
+                setValue('slug', slugTransform(value.title), {shouldValidate: true})
+            }
+        })
+        return ()=>{
+            subscription.unsubscribe()
+        }
+    }, [watch, slugTransform, setValue])
   return (
     <div>
       
