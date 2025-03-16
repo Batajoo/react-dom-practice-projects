2222692491ea920901f55b4ab872369de46e4368
Sanjiv <sanjivbatajoo@gmail.com>
2025-03-16 00:20:51 -0500

    react router using data mode

--- 13MegaBlogFullRevision/src/main.jsx
+++ 13MegaBlogFullRevision/src/main.jsx
@@ -4,16 +4,54 @@ import { Provider } from 'react-redux'
 import store from './store/store.js'
 import './index.css'
 import App from './App.jsx'
-import { createBrowserRouter } from 'react-router'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import {AllPosts, Home, Login, SignupPage} from "./pages"
+import AuthLayout from "./components/AuthLayout.jsx"
 
 const router = createBrowserRouter([
-  
+  {
+    path: "/",
+    element: <App/>,
+    children: [
+      {
+        path: "/",
+        element: <Home/>
+      },
+      {
+        path:"/login",
+        element: (
+          <AuthLayout authentication={false}>
+            <Login/>
+          </AuthLayout>
+        )
+      },
+      {
+        path: "/signup",
+        element: (
+          <AuthLayout authentication={false}>
+            <SignupPage/>
+          </AuthLayout>
+        )
+      },
+      {
+        path: "/all-posts",
+        element: (
+          <AuthLayout authentication>
+            <AllPosts/>
+          </AuthLayout>
+        )
+      },
+      {
+        
+      }
+    ]
+  }
 ])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+      <Provider store={store}>
+        <RouterProvider router={router}/>
+      </Provider>
   </StrictMode>,
 )
---
+++ 13MegaBlogFullRevision/src/pages/index.js
@@ -0,0 +1,17 @@
+import Home from "./Home"
+import AddPost from "./AddPost"
+import AllPosts from "./AllPosts"
+import EditPost from "./EditPost"
+import Login from "./Login"
+import Post from "./Post"
+import SignupPage from "./Signup"
+
+export {
+    Home,
+    AddPost,
+    AllPosts,
+    EditPost,
+    Login,
+    Post,
+    SignupPage
+}
